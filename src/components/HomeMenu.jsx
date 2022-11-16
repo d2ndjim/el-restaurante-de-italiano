31bf573facce8cd96e10125af7f6a81cae6f575b
Lekan <jimoridwanlekan@gmail.com>
2022-11-16 13:16:08 +0100

    Add styling to home menu

--- src/components/HomeMenu.jsx
+++ src/components/HomeMenu.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchHomeMenu, selectMenus } from '../features/menu/menuSlice';
+import Styles from '../pages/Home.module.css';
 
 function HomeMenu() {
   const dispatch = useDispatch();
@@ -13,23 +14,27 @@ function HomeMenu() {
   return (
     <div className="pb-24">
       <div className="text-center">
-        <h1 className="text-8xl font-bold text-[#C8A97E]">Specialties</h1>
-        <h1 className="font-bold text-7xl">Our Menu</h1>
+        <h1 className="text-4xl md:text-8xl font-bold text-[#C8A97E]">
+          Specialties
+        </h1>
+        <h1 className="text-5xl font-bold md:text-7xl">Our Menu</h1>
       </div>
-      <div className="text-center py-10 px-32">
-        <div className="grid grid-cols-2 border-[1px] border-bordercolor">
+      <div className="text-center py-10 px-4 md:px-32">
+        <div
+          className={`${Styles.card} flex flex-col md:grid md:grid-cols-2 gap-2 border-[1px]`}
+        >
           {menus.map((menu) => (
-            <div key={menu.id} className="flex">
-              <div className="w-[50%]">
+            <div key={menu.id} className="flex flex-col md:flex-row">
+              <div className="w-full md:w-[50%]">
                 <img
                   src={menu.image}
                   alt={menu.name}
-                  className="h-[300px] w-full"
+                  className="h-[300px] w-full rounded"
                 />
               </div>
-              <div className="flex flex-col items-start w-[50%] px-8 py-4">
-                <div className="flex flex-row justify-between">
-                  <h3 className="text-xl font-semibold text-start text-paragraph mr-8">
+              <div className="flex flex-col justify-between gap-6 md:items-start md:w-[50%] px-3 py-4">
+                <div className="flex justify-between">
+                  <h3 className="text-2xl md:text-xl font-semibold text-start mr-8 ">
                     {menu.name}
                   </h3>
                   <p className="text-2xl font-semibold text-[#C8A97E]">
@@ -37,12 +42,10 @@ function HomeMenu() {
                     {Number(menu.price)}
                   </p>
                 </div>
-                <p className="text-paragraph text-start w-[70%] mt-2">
-                  {menu.description}
-                </p>
+                <p className="text-start text-xl md:text-base md:w-[70%] mt-2">{menu.description}</p>
                 <button
                   type="button"
-                  className="border-solid border-1 bg-[#C8A97E] py-2 px-4 mt-2 text-white rounded-[5px]"
+                  className="border-solid text-[20px] md:text-xl border-1 bg-[#C8A97E] md:py-3 py-3 px-4 md:px-6 mt-2 text-white rounded-lg"
                 >
                   Order now
                 </button>
