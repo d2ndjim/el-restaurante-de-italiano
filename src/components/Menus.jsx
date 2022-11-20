424e881188d524df6ba68dd16833a4e67cf5498d
Lekan <jimoridwanlekan@gmail.com>
2022-11-20 18:01:35 +0100

    Render menus in menu page

---
+++ src/components/Menus.jsx
@@ -0,0 +1,403 @@
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import { useAuth } from '../config/ProtectedRoutes';
+import { fetchDessert, selectDesserts } from '../features/menu/dessertSlice';
+import { fetchDrinks, selectDrinks } from '../features/menu/drinkSlice';
+import { fetchNonVegetarian, selectNonVegetarian } from '../features/menu/nonVegetarianSlice';
+import { fetchVegetarian, selectVegetarian } from '../features/menu/vegetarianSlice';
+import { fetchWines, selectWines } from '../features/menu/wineSlice';
+import { makeOrder, selectOrders, reset } from '../features/order/orderSlice';
+import Spinner from './Spinner';
+
+function Menus() {
+  const dispatch = useDispatch();
+  const [isVegetarian, setIsVegetarian] = useState(true);
+  const [isNonVegetarian, setIsNonVegetarian] = useState(false);
+  const [isDessert, setIsDessert] = useState(false);
+  const [isDrinks, setIsDrinks] = useState(false);
+  const [isWines, setIsWines] = useState(false);
+  const [saved, setSaved] = useState(false);
+  // const [isHover, setIsHover] = useState(false);
+
+  // const handleMouseEnter = () => {
+  //   setIsHover(true);
+  // };
+  // const handleMouseLeave = () => {
+  //   setIsHover(false);
+  // };
+
+  const {
+    isLoading, isError, isSuccess, message,
+  } = useSelector(selectOrders);
+
+  const isAuth = useAuth();
+
+  const handleClick = (id) => {
+    if (isAuth) {
+      Swal.fire({
+        title: 'Confirm Order?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#C8A97E',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Confirm!',
+      }).then((result) => {
+        if (result.isConfirmed) {
+          dispatch(makeOrder(id));
+        }
+      });
+    } else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Please login to make order!',
+      });
+    }
+  };
+
+  function handleVegetarian() {
+    setIsVegetarian(true);
+    setIsNonVegetarian(false);
+    setIsDessert(false);
+    setIsDrinks(false);
+    setIsWines(false);
+  }
+
+  function handleNonVegetarian() {
+    setIsVegetarian(false);
+    setIsNonVegetarian(true);
+    setIsDessert(false);
+    setIsDrinks(false);
+    setIsWines(false);
+  }
+
+  function handleDessert() {
+    setIsVegetarian(false);
+    setIsNonVegetarian(false);
+    setIsDessert(true);
+    setIsDrinks(false);
+    setIsWines(false);
+  }
+
+  function handleDrink() {
+    setIsVegetarian(false);
+    setIsNonVegetarian(false);
+    setIsDessert(false);
+    setIsDrinks(true);
+    setIsWines(false);
+  }
+
+  function handleWine() {
+    setIsVegetarian(false);
+    setIsNonVegetarian(false);
+    setIsDessert(false);
+    setIsDrinks(false);
+    setIsWines(true);
+  }
+
+  const vegetarians = useSelector(selectVegetarian);
+  const nonVegetarians = useSelector(selectNonVegetarian);
+  const desserts = useSelector(selectDesserts);
+  const wines = useSelector(selectWines);
+  const drinks = useSelector(selectDrinks);
+
+  useEffect(() => {
+    dispatch(fetchVegetarian());
+    dispatch(fetchNonVegetarian());
+    dispatch(fetchDessert());
+    dispatch(fetchDrinks());
+    dispatch(fetchWines());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (isError) {
+      Swal.fire('Error!', message, 'error');
+    }
+
+    if (isSuccess) {
+      setSaved(true);
+      Swal.fire('Confirmed!', 'Your order has been confirmed.', 'success');
+    }
+
+    dispatch(reset());
+  }, [isError, isSuccess, message, dispatch]);
+
+  if (saved) {
+    return <Navigate to="/orders" replace />;
+  }
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  return (
+    <div className="">
+      <div className="flex flex-col gap-2 px-4 md:grid md:grid-cols-5 md:gap-1 md:px-32 md:pb-9">
+        <button
+          type="button"
+          className="py-4 md:py-6 text-xl font-bold hover:bg-[#C8A97E] hover:text-white"
+          onClick={handleVegetarian}
+          // onMouseEnter={handleMouseEnter}
+          // onMouseLeave={handleMouseLeave}
+          style={
+            isVegetarian
+              ? { backgroundColor: '#C8A97E', color: 'white' }
+              : { backgroundColor: '#FFFFFF', color: 'black' }
+          }
+        >
+          Vegetarian
+        </button>
+        <button
+          type="button"
+          className="py-4 md:py-6 text-xl font-bold hover:bg-[#C8A97E] hover:text-white"
+          onClick={handleNonVegetarian}
+          style={
+            isNonVegetarian
+              ? { backgroundColor: '#C8A97E', color: 'white' }
+              : { backgroundColor: '#FFFFFF', color: 'black' }
+          }
+        >
+          Non Vegetarian
+        </button>
+        <button
+          type="button"
+          className="py-4 md:py-6 text-xl font-bold hover:bg-[#C8A97E] hover:text-white"
+          onClick={handleDessert}
+          style={
+            isDessert
+              ? { backgroundColor: '#C8A97E', color: 'white' }
+              : { backgroundColor: '#FFFFFF', color: 'black' }
+          }
+        >
+          Desserts
+        </button>
+        <button
+          type="button"
+          className="py-4 md:py-6 text-xl font-bold hover:bg-[#C8A97E] hover:text-white"
+          onClick={handleDrink}
+          style={
+            isDrinks
+              ? { backgroundColor: '#C8A97E', color: 'white' }
+              : { backgroundColor: '#FFFFFF', color: 'black' }
+          }
+        >
+          Drinks
+        </button>
+        <button
+          type="button"
+          className="py-4 md:py-6 text-white text-xl font-bold hover:bg-[#C8A97E] hover:text-white"
+          onClick={handleWine}
+          style={
+            isWines
+              ? { backgroundColor: '#C8A97E', color: 'white' }
+              : { backgroundColor: '#FFFFFF', color: 'black' }
+          }
+        >
+          Wines
+        </button>
+      </div>
+      <div className="text-center py-10 px-4 md:px-32 mb-28">
+        {isVegetarian && (
+          <div
+            className="flex flex-col md:grid md:grid-cols-2 gap-2 border-[1px]"
+            data-aos="fade-up"
+          >
+            {vegetarians.map((menu) => (
+              <div key={menu.id} className="flex flex-col md:flex-row">
+                <div className="w-full md:w-[50%]">
+                  <img
+                    src={menu.image}
+                    alt={menu.name}
+                    className="h-[300px] w-full rounded"
+                  />
+                </div>
+                <div className="flex flex-col justify-between gap-6 md:items-start md:w-[50%] px-3 py-4">
+                  <div className="flex justify-between">
+                    <h3 className="text-2xl md:text-xl font-semibold text-start mr-8 ">
+                      {menu.name}
+                    </h3>
+                    <p className="text-2xl font-semibold text-[#C8A97E]">
+                      $
+                      {Number(menu.price)}
+                    </p>
+                  </div>
+                  <p className="text-start text-xl md:text-base md:w-[70%] mt-2">
+                    {menu.description}
+                  </p>
+                  <button
+                    type="button"
+                    className="border-solid text-[20px] md:text-xl border-1 bg-[#C8A97E] md:py-3 py-3 px-4 md:px-6 mt-2 text-white rounded-lg"
+                    onClick={() => handleClick(menu.id)}
+                  >
+                    Order now
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+        {isNonVegetarian && (
+          <div
+            className="flex flex-col md:grid md:grid-cols-2 gap-2 border-[1px]"
+            data-aos="fade-up"
+          >
+            {nonVegetarians.map((menu) => (
+              <div key={menu.id} className="flex flex-col md:flex-row">
+                <div className="w-full md:w-[50%]">
+                  <img
+                    src={menu.image}
+                    alt={menu.name}
+                    className="h-[300px] w-full rounded"
+                  />
+                </div>
+                <div className="flex flex-col justify-between gap-6 md:items-start md:w-[50%] px-3 py-4">
+                  <div className="flex justify-between">
+                    <h3 className="text-2xl md:text-xl font-semibold text-start mr-8 ">
+                      {menu.name}
+                    </h3>
+                    <p className="text-2xl font-semibold text-[#C8A97E]">
+                      $
+                      {Number(menu.price)}
+                    </p>
+                  </div>
+                  <p className="text-start text-xl md:text-base md:w-[70%] mt-2">
+                    {menu.description}
+                  </p>
+                  <button
+                    type="button"
+                    className="border-solid text-[20px] md:text-xl border-1 bg-[#C8A97E] md:py-3 py-3 px-4 md:px-6 mt-2 text-white rounded-lg"
+                    onClick={() => handleClick(menu.id)}
+                  >
+                    Order now
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+        {isDessert && (
+          <div
+            className="flex flex-col md:grid md:grid-cols-2 gap-2 border-[1px]"
+            data-aos="fade-up"
+          >
+            {desserts.map((menu) => (
+              <div key={menu.id} className="flex flex-col md:flex-row">
+                <div className="w-full md:w-[50%]">
+                  <img
+                    src={menu.image}
+                    alt={menu.name}
+                    className="h-[300px] w-full rounded"
+                  />
+                </div>
+                <div className="flex flex-col justify-between gap-6 md:items-start md:w-[50%] px-3 py-4">
+                  <div className="flex justify-between">
+                    <h3 className="text-2xl md:text-xl font-semibold text-start mr-8 ">
+                      {menu.name}
+                    </h3>
+                    <p className="text-2xl font-semibold text-[#C8A97E]">
+                      $
+                      {Number(menu.price)}
+                    </p>
+                  </div>
+                  <p className="text-start text-xl md:text-base md:w-[70%] mt-2">
+                    {menu.description}
+                  </p>
+                  <button
+                    type="button"
+                    className="border-solid text-[20px] md:text-xl border-1 bg-[#C8A97E] md:py-3 py-3 px-4 md:px-6 mt-2 text-white rounded-lg"
+                    onClick={() => handleClick(menu.id)}
+                  >
+                    Order now
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+        {isDrinks && (
+          <div
+            className="flex flex-col md:grid md:grid-cols-2 gap-2 border-[1px]"
+            data-aos="fade-up"
+          >
+            {drinks.map((menu) => (
+              <div key={menu.id} className="flex flex-col md:flex-row">
+                <div className="w-full md:w-[50%]">
+                  <img
+                    src={menu.image}
+                    alt={menu.name}
+                    className="h-[300px] w-full rounded"
+                  />
+                </div>
+                <div className="flex flex-col justify-between gap-6 md:items-start md:w-[50%] px-3 py-4">
+                  <div className="flex justify-between">
+                    <h3 className="text-2xl md:text-xl font-semibold text-start mr-8 ">
+                      {menu.name}
+                    </h3>
+                    <p className="text-2xl font-semibold text-[#C8A97E]">
+                      $
+                      {Number(menu.price)}
+                    </p>
+                  </div>
+                  <p className="text-start text-xl md:text-base md:w-[70%] mt-2">
+                    {menu.description}
+                  </p>
+                  <button
+                    type="button"
+                    className="border-solid text-[20px] md:text-xl border-1 bg-[#C8A97E] md:py-3 py-3 px-4 md:px-6 mt-2 text-white rounded-lg"
+                    onClick={() => handleClick(menu.id)}
+                  >
+                    Order now
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+        {isWines && (
+          <div
+            className="flex flex-col md:grid md:grid-cols-2 gap-2 border-[1px]"
+            data-aos="fade-up"
+          >
+            {wines.map((menu) => (
+              <div key={menu.id} className="flex flex-col md:flex-row">
+                <div className="w-full md:w-[50%]">
+                  <img
+                    src={menu.image}
+                    alt={menu.name}
+                    className="h-[300px] w-full rounded"
+                  />
+                </div>
+                <div className="flex flex-col justify-between gap-6 md:items-start md:w-[50%] px-3 py-4">
+                  <div className="flex justify-between">
+                    <h3 className="text-2xl md:text-xl font-semibold text-start mr-8 ">
+                      {menu.name}
+                    </h3>
+                    <p className="text-2xl font-semibold text-[#C8A97E]">
+                      $
+                      {Number(menu.price)}
+                    </p>
+                  </div>
+                  <p className="text-start text-xl md:text-base md:w-[70%] mt-2">
+                    {menu.description}
+                  </p>
+                  <button
+                    type="button"
+                    className="border-solid text-[20px] md:text-xl border-1 bg-[#C8A97E] md:py-3 py-3 px-4 md:px-6 mt-2 text-white rounded-lg"
+                    onClick={() => handleClick(menu.id)}
+                  >
+                    Order now
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Menus;
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Menus from '../components/Menus';
 import MobileNav from '../components/MobileNav';
@@ -10,11 +11,16 @@ function Menu() {
         <Header />
         <MobileNav />
         <div className="mt-80 md:mt-52 flex flex-col place-items-center gap-4">
-          <h1 className="text-white text-5xl md:text-7xl font-extrabold">Specialties</h1>
-          <Link to="/" className="text-2xl text-white font-semibold">Home</Link>
+          <h1 className="text-white text-5xl md:text-7xl font-extrabold">
+            Specialties
+          </h1>
+          <Link to="/" className="text-2xl text-white font-semibold">
+            Home
+          </Link>
         </div>
       </div>
       <Menus />
+      <Footer />
     </div>
   );
 }
