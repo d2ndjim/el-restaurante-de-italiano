f0f020b170b7022c152bb1032fa075284b8739b6
Lekan <jimoridwanlekan@gmail.com>
2022-11-20 17:06:45 +0100

    Update store

--- src/app/store.js
+++ src/app/store.js
@@ -1,13 +1,23 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
-import menuReducer from '../features/menu/menuSlice';
+import menuReducer from '../features/menu/homeMenuSlice';
 import orderReducer from '../features/order/orderSlice';
+import vegetarianReducer from '../features/menu/vegetarianSlice';
+import nonVegetarianReducer from '../features/menu/nonVegetarianSlice';
+import dessertReducer from '../features/menu/dessertSlice';
+import wineReducer from '../features/menu/wineSlice';
+import drinkReducer from '../features/menu/drinkSlice';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     homeMenu: menuReducer,
     order: orderReducer,
+    vegetarian: vegetarianReducer,
+    nonVegetarian: nonVegetarianReducer,
+    dessert: dessertReducer,
+    wine: wineReducer,
+    drink: drinkReducer,
   },
 });
 
